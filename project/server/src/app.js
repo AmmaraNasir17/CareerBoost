23721e6302aad6daea6e0dc3a51191ce192a735c
AbdulHadi117 <117abdulhadi@gmail.com>
2026-03-05 16:07:58 +0500

    Connected Routes to Server

--- project/server/src/app.js
+++ project/server/src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 require("dotenv").config();
 
+const authRoutes = require("./routes/auth.routes");
+
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("CareerBoost Server Running");
