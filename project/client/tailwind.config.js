0f8964af08ad41ac25fb7cd3dcbf8c19b1fb374e
AmmaraNasir17 <ammaranasir17@gmail.com>
2026-03-06 11:04:45 +0500

    Tailwind Configuration JS File

---
+++ project/client/tailwind.config.js
@@ -0,0 +1,16 @@
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,jsx}"
+];
+export const theme = {
+  extend: {
+    colors: {
+      primary: '#1E40AF',
+      secondary: '#0F766E'
+    },
+    scale: {
+      103: '1.03'
+    }
+  }
+};
+export const plugins = [];
