c86d230459803528e4e0603f5345543e64e43251
AbdulHadi117 <117abdulhadi@gmail.com>
2026-03-12 12:20:13 +0500

    Fixed CORS for backend to must allow frontend interaction

--- project/server/src/app.js
+++ project/server/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
 const authRoutes = require("./routes/auth.routes");
@@ -7,6 +8,10 @@ const testRoutes = require("./routes/test.routes");
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/test", testRoutes);
