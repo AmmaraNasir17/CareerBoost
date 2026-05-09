d8e691928f35e240576a07a31f79cb7ba7506a0d
AbdulHadi117 <117abdulhadi@gmail.com>
2026-05-10 04:12:00 +0500

    Updated role middleware for authentication module

--- project/server/src/middleware/role.middleware.js
+++ project/server/src/middleware/role.middleware.js
@@ -1,13 +1,11 @@
-module.exports = function (roles) {
-
+module.exports = function (...allowedRoles) {
   return (req, res, next) => {
-
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({
-        message: "Access denied"
-      });
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
-
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Access denied" });
+    }
     next();
   };
 };
