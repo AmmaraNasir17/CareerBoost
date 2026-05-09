8069ede08e9e770feb200e14196e67fddf4b28a2
AbdulHadi117 <117abdulhadi@gmail.com>
2026-05-10 04:12:30 +0500

    updated application routes with additional functionalities

--- project/server/src/routes/application.routes.js
+++ project/server/src/routes/application.routes.js
@@ -1,41 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const applicationController = require("../controllers/application.controller");
-
+const applicationController = require("../controllers/applications/application.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 const roleMiddleware = require("../middleware/role.middleware");
 
-// View own applications
-router.get(
-  "/my-applications",
-  authMiddleware,
-  roleMiddleware(["applier"]),
-  applicationController.getMyApplications
-);
-
-// View my applicants
-router.get(
-  "/my-applicants",
-  authMiddleware,
-  roleMiddleware(["recruiter"]),
-  applicationController.getMyApplicants
-);
-
-// View applications for a job (recruiter)
-router.get(
-  "/job/:jobId/applications",
-  authMiddleware,
-  roleMiddleware(["recruiter"]),
-  applicationController.getApplicationsForJob
-);
-
-// Update application status (recruiter)
-router.put(
-  "/:applicationId/status",
-  authMiddleware,
-  roleMiddleware(["recruiter"]),
-  applicationController.updateApplicationStatus
-);
+router.get("/my", authMiddleware, roleMiddleware("applier"), applicationController.getMyApplications);
+router.get("/all", authMiddleware, roleMiddleware("recruiter"), applicationController.getAllApplicants);
+router.patch("/:id/status", authMiddleware, roleMiddleware("recruiter"), applicationController.updateStatus);
 
 module.exports = router;
