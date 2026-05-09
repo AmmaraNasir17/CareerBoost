07e4bd1713e8c190167fd5d39d6ca2dc257d97de
AbdulHadi117 <117abdulhadi@gmail.com>
2026-05-10 04:07:49 +0500

    Updated authentication routes

--- project/server/src/routes/auth.routes.js
+++ project/server/src/routes/auth.routes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const authController = require("../controllers/auth.controller");
+const authController = require("../controllers/auth/auth.controller");
+const passwordController = require("../controllers/auth/password.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.get("/me", authMiddleware, authController.getUserData);
+router.get("/me", authMiddleware, authController.getMe);
+
+router.post("/forgot-password", passwordController.requestPasswordReset);
+router.post("/reset-password", passwordController.resetPassword);
+router.post("/change-password", authMiddleware, passwordController.changePassword);
+
 module.exports = router;
